8c70b66219fba6ac03a632fb25a6029de7fddf19
DavidTibet <vodopyanovdaniil@gmail.com>
2021-06-12 20:54:28 +0300

    Web-server

--- main.tf
+++ main.tf
@@ -8,24 +8,18 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
-}
-
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
+resource "aws_instance" "example" {
+  ami                    = "ami-00399ec92321828f5"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = "server2"
+  depends_on             = [aws_security_group.instance]
+ 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 resource "aws_security_group" "instance" {
@@ -40,15 +34,3 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name 
-  min_size             = 2
-  max_size             = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true 
-  }
-
-}
